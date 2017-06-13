d05833b817d7ceea3d4af83311ea7574713e9e0a
snupreme <blakebein@gmail.com>
2017-06-12 21:10:48 -0700

    Create CryptoStalkerChrome.py

--- CryptoStalkerChrome.py
+++ CryptoStalkerChrome.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 import time 
+import colorama
+from colorama import Fore, Back, Style
+import time
+colorama.init()
 driver = webdriver.Chrome()
 def getCurrentTime():
     return time.strftime("%H:%M:%S")
 url = 'https://coinbase.com/charts/'
-print("Made By @snupreme aka turtl")
-print("[{}] Enabling CryptoStalker".format(getCurrentTime()))
+print(Fore.MAGENTA +"Made By @snupreme aka turtl")
+print(Fore.YELLOW +"[{}] Enabling CryptoStalker".format(getCurrentTime()))
 driver.get(url)
 var = 99999999 
 count = 0                                         
@@ -18,9 +22,9 @@ while count < var:
 	bitcoinhourly = driver.find_element_by_xpath("""//*[@id="chart_react"]/div/div[1]/div[2]/div/div[1]""").click()
 	time.sleep(.3)
 	bitcoinchange = driver.find_element_by_xpath("""//*[@id="chart_react"]/div/div[2]/div/div[1]/div/div[2]/div[1]""")
-	print("########################################")
-	print("[{}] Current Bitcoin Price is".format(getCurrentTime()), bitcoin.text)
-	print("Bitcoin Hourly Price Change: "+bitcoinchange.text)
+	print(Fore.CYAN + "########################################")
+	print(Fore.YELLOW + "[{}] Current Bitcoin Price is".format(getCurrentTime()), bitcoin.text)
+	print(Fore.YELLOW + "Bitcoin Hourly Price Change: "+bitcoinchange.text)
 	time.sleep(2)
 	print(" ")
 	#ether
@@ -29,9 +33,9 @@ while count < var:
 	time.sleep(.5)
 	etherphourly = driver.find_element_by_xpath("""//*[@id="chart_react"]/div/div[1]/div[2]/div/div[1]""").click()
 	etherchange = driver.find_element_by_xpath("""//*[@id="chart_react"]/div/div[2]/div/div[1]/div/div[2]/div[1]""")
-	print("[{}] Current Ethereum Price is".format(getCurrentTime()), ethereum.text)
-	print("Ethereum Hourly Price Change: "+etherchange.text)
-	print("########################################")
+	print(Fore.YELLOW + "[{}] Current Ethereum Price is".format(getCurrentTime()), ethereum.text)
+	print(Fore.YELLOW + "Ethereum Hourly Price Change: "+etherchange.text)
+	print(Fore.CYAN + "########################################")
 	time.sleep(3600)#the amount of seconds it waits to check prices set to one hour	
 	count = count + 1
 driver.quit()
